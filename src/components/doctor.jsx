0b3b3202db72b72d7d0aa292de71ba0b7d4fb25b
johnsonsirv <johnsonsirv@gmail.com>
2020-08-06 12:31:57 +0100

    refac: make the entire dotor card clickable

--- src/components/doctor.jsx
+++ src/components/doctor.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Doctor = props => {
   const {
     doctor: { firstname, lastname, city, email, username },
   } = props;
   return (
-    <div className="doctorCard">
-      <h2>{`Dr. ${firstname} ${lastname}`}</h2>
-      <p>
-        <i>Location: </i>
-        {city}
-      </p>
-      <p>
-        <i>{email}</i>
-      </p>
-      <Link to={`/book-appointment/${username}`}>Schedule Appointment</Link>
-    </div>
+    <NavLink to={`/book-appointment/${username}`}>
+      <div className="doctorCard">
+        <h2>{`Dr. ${firstname} ${lastname}`}</h2>
+        <p>
+          <i>Location: </i>
+          {city}
+        </p>
+        <p>
+          <i>{email}</i>
+        </p>
+        <button href={`/book-appointment/${username}`} type="button">
+          Schedule Appointment
+        </button>
+      </div>
+    </NavLink>
   );
 };
 
